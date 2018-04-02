f47022c69b0f37bb346fa47fd772fa833ddaed6f
krachzack <hello@phstadler.com>
2018-04-02 20:08:14 +0200

    Triangles to be rendered in textures must now be half a squarepixel in size
    squarepixel in to texture size scaled uv space

--- src/position_tex.rs
+++ src/position_tex.rs
@@ -11,12 +11,14 @@ pub fn position_tex(entity: &Entity, width: usize, height: usize, island_bleed: 
     // 15 for 4096x4096, 9 for 2048x2048, 6 for 1024x1024, 3 for everything below
     //let island_bleed = (width / 1024) * 3 + 3;
 
-    let min_area = EPSILON;
+    // Minimum of 0.1 squarepixels in UV space
+    let uv_space_min_area = 0.1;
 
     let uv_triangles = entity.mesh
         .triangles()
-        .filter(|t| t.area() > min_area)
-        .map(|t| triangle_into_uv_image_space(t, width, height));
+        .filter(|t| t.area() > EPSILON)
+        .map(|t| triangle_into_uv_image_space(t, width, height))
+        .filter(|t| t.area() > uv_space_min_area);
 
     // Before drawing the triangles, draw the outlines in a thick stroke to
     // ensure there will be margins around the UV islands.
@@ -42,8 +44,9 @@ pub fn position_tex(entity: &Entity, width: usize, height: usize, island_bleed: 
     // Cannot clone the iterator because the closures cannot be cloned, creating it again
     let uv_triangles = entity.mesh
         .triangles()
-        .filter(|t| t.area() > min_area) // Should be at least half a pixel in size
-        .map(|t| triangle_into_uv_image_space(t, width, height));
+        .filter(|t| t.area() > EPSILON)
+        .map(|t| triangle_into_uv_image_space(t, width, height))
+        .filter(|t| t.area() > uv_space_min_area);
 
     // Next, draw the insides of the triangles, the real star of the show
     uv_triangles
